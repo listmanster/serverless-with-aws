70e98593554ddab8c799c2e7132c246232f9da60
listmanster <listmanster@gmail.com>
2020-11-10 10:11:13 +0530

    Updates

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,8 +1,13 @@
 module.exports.handler = (event, ctx, done) => {
+  
+  const todo = [
+    {id: 2, name: 'single', status: 'single'}
+  ];
+
+  console.log(JSON.stringify(event, null, 2));
+
   done(null, {
     statusCode: 200,
-    body: JSON.stringify({data: {
-      id: 1, name: 'clean up', status: 'open'
-    }})
+    body: JSON.stringify({data: todo})
   })
 }
